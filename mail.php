cdaa2b6b5a18416697717e43f596a7f8ab1b2177
washington mugo <washiemugo@gmail.com>
2019-09-22 16:35:12 +0300

    configured email

--- mail.php
+++ mail.php
@@ -5,10 +5,10 @@
             $mailheader = "From: ".$_POST["email"]."\r\n"; 
             $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
             $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-            $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
-            $MESSAGE_BODY = "Subject: ".$_POST["subject"].""; 
-            $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
-            $MESSAGE_BODY .= "Message ".nl2br($_POST["message"]).""; 
+            $MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
+            $MESSAGE_BODY = "Subject: ".$_POST["subject"]."<br>"; 
+            $MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
+            $MESSAGE_BODY .= "Message ".nl2br($_POST["message"])."<br>"; 
             mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
         echo "Message Successfully Sent !";
     } else { 
